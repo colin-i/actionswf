4c133b2511f4a96af05f4eab196ab57fd981cee6
bc <costin.botescu@gmail.com>
2022-01-25 13:29:08 +0200

    swf perms 664 (ruffle)

--- include/lin.h
+++ include/lin.h
@@ -2,7 +2,7 @@
 Const flag_O_BINARY=0
 Const flag_O_CREAT=0x0040
 
-const S_IRWXU=0x1C0
-const S_IRGRP=0x20;const S_IXGRP=0x8
-const S_IROTH=0x4;const S_IXOTH=0x1
-Const flag_pmode=S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH
+const S_IRUSR=0x100;const S_IWUSR=0x80
+const S_IRGRP=0x20;const S_IWGRP=0x10
+const S_IROTH=0x4;const S_IWOTH=0x2
+Const flag_pmode=S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH
