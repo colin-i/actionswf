f4b8fb446bc522877ad87623c379b40237f6ef9c
bc <costin.botescu@gmail.com>
2024-05-12 14:58:10 +0300

    must test against zero for fwrite

--- include/action.h
+++ include/action.h
@@ -1,26 +1,35 @@
 
 const no_pointer=0  #NULL
 
-const function_action=0xf3033030
-const call_action_left=0xf1011010
-const ActionDefineLocal=0x3C
-const ActionSetMember=0x4F
-const math_end=0xffFFffFF
+const function_action=0xf3033030   #1
+const call_action_left=0xf1011010  #1
+const ActionSetVariable=0x1D       #1
+const ActionDelete=0x3A            #1
+const ActionDelete2=0x3B           #1
+const ActionDefineLocal=0x3C       #1
+const ActionDefineLocal2=0x41      #1
+const ActionSetMember=0x4F         #1
 
 const block_end=0xfbBBbbBB
-
 const args_end=0xfeEEeeEE
+const math_end=0xffFFffFF          #1
 
-#M structure
-const square_bracket_start=0xf5055050
-const member_end=0xff0FF0f0
+const square_bracket_start=0xf5055050  #M
+const parenthesis_start=0xf8088080     #RG
+const member_end=0xff0FF0f0            #M
 
-#RG structure
-    const ap_double=6
-    const ap_Integer=7
-const ActionGetVariable=0x1C
-const ActionGetMember=0x4E
-
+const ap_double=6              #RG
+const ap_Integer=7             #RG
+const ActionSubtract=0x0B      #O
+const ActionMultiply=0x0C      #O
+const ActionDivide=0x0D        #O
+const ActionGetVariable=0x1C   #RG
+const ActionModulo=0x3F        #O
+const ActionAdd2=0x47          #O
+const ActionGetMember=0x4E     #RG
+const ActionBitAnd=0x60        #O
+const ActionBitOr=0x61         #O
+const ActionBitXor=0x62        #O
 
 
 const ActionEndFlag=0
@@ -29,29 +38,20 @@ const ActionPreviousFrame=0x05
 const ActionPlay=0x06
 const ActionStop=0x07
 #const ActionAdd=0x0A
-const ActionSubtract=0x0B
-const ActionMultiply=0x0C
-const ActionDivide=0x0D
 const ActionAnd=0x10
 const ActionOr=0x11
 const ActionNot=0x12
 const ActionPop=0x17
 const ActionToInteger=0x18
-const ActionSetVariable=0x1D
 const ActionTrace=0x26
 const ActionRandomNumber=0x30
 const ActionCharToAscii=0x32
 const ActionAsciiToChar=0x33
-const ActionDelete=0x3A
-const ActionDelete2=0x3B
 const ActionCallFunction=0x3D
 const ActionReturn=0x3E
-const ActionModulo=0x3F
 const ActionNewObject=0x40
-const ActionDefineLocal2=0x41
 const ActionTypeOf=0x44
 const ActionEnumerate=0x46
-const ActionAdd2=0x47
 const ActionLess2=0x48
 const ActionEquals2=0x49
 const ActionPushDuplicate=0x4C
@@ -59,9 +59,6 @@ const ActionIncrement=0x50
 const ActionDecrement=0x51
 const ActionCallMethod=0x52
 const ActionNewMethod=0x53
-const ActionBitAnd=0x60
-const ActionBitOr=0x61
-const ActionBitXor=0x62
 const ActionBitLShift=0x63
 const ActionBitRShift=0x64
 const ActionBitURShift=0x65
@@ -85,7 +82,6 @@ const call_action_right=0xf2022020
 const new_action=0xf4044040
 const mixt_equal=0xf6066060
 const compare_action=0xf7077070
-const parenthesis_start=0xf8088080
 const break_flag=0xf9099090
 const continue_flag=0xfa0AA0a0
 #
