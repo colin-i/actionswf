0604462fa2ed0705cc44970ff2f1792726be4c6e
bc <costin.botescu@gmail.com>
2024-05-18 13:56:55 +0300

    x restore

--- include/flags.h
+++ include/flags.h
@@ -5,7 +5,9 @@ const debug_alt=0x2
 const debug_titles=0x4
 const debug_x=0x8
 const debug_x_pad=0x10
-const debug_xx=debug_x|debug_x_pad
 
-const flags_forin1=0x20
-const flags_framesAsShows=0x40
+const flag_forin1=0x20
+const flag_framesAsShows=0x40
+#const flag_x=0x80
+
+const flags_x=debug_x|debug_x_pad ;#|flag_x
