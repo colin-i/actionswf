9b84dcbcd7be9fb7aead99485eb1ca705968f278
bc <costin.botescu@gmail.com>
2025-05-04 10:09:19 +0300

    or if no __has_builtin

--- oad/safe.h
+++ oad/safe.h
@@ -1,14 +1,13 @@
 
 //typedef long long long;//on windows but later
 
-#if defined __has_builtin  //gcc/clang
-#  if ! __has_builtin (__builtin_malloc)  //not the same as #ifndef __builtin_malloc
+//gcc/clang with __has_builtin    //not the same as #ifndef __builtin_malloc
+#if (! defined(__has_builtin)) || (! __has_builtin (__builtin_malloc))
 void*malloc(long unsigned int);
-#  endif
-#  if ! __has_builtin (__builtin_realloc)
+#endif
+#if (! defined(__has_builtin)) || (! __has_builtin (__builtin_realloc))
 void*realloc(void*,long unsigned int);
-#  endif
-#  if ! __has_builtin (__builtin_strlen)
+#endif
+#if (! defined(__has_builtin)) || (! __has_builtin (__builtin_strlen))
 long unsigned int strlen(const char*);
-#  endif
 #endif
