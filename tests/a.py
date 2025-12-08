3ecc2226292c34406c85f0d37edc252f93c62f3b
bc <costin.botescu@gmail.com>
2025-12-08 11:10:14 +0200

    a.py test

---
+++ tests/a.py
@@ -0,0 +1,26 @@
+
+import actionswf
+
+def text(name):
+	return open(name).read()
+
+actionswf.xlog_pad_set(0x30)
+
+import sys
+if len(sys.argv)==1:
+	f=actionswf.flags.prexx|actionswf.flag.pre_x_pad|actionswf.flag.pre_write_no
+else:
+	print("xxu|titles")
+	f=actionswf.flagss.xxu|actionswf.flag.titles
+
+actionswf.new_ex("a.swf",0x80,0x80,0x050607,2,f)
+action(text("./aoc/a"))
+presprite=actionswf.sprite_new()
+actionswf.action_sprite(presprite,"trace('b'+this.value)")
+actionswf.action_init_sprite(presprite,"./aoc/b")
+sprite=actionswf.sprite_done(presprite)
+actionswf.exports_add(sprite,"carSymbol")
+
+actionswf.done()
+
+exit(0)
